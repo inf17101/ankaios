c1a2377a51e4da6b5bbdbb71c8b2f37edce8462d
Oliver <42932060+inf17101@users.noreply.github.com>
2024-05-21 16:16:17 +0200

    Change agent main to test cache

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -52,7 +52,7 @@ const BUFFER_SIZE: usize = 20;
 #[tokio::main]
 async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
+    println!("Start agent!");
     let args = cli::parse();
 
     log::debug!(
